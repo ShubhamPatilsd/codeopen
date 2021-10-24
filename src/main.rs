bb5d07fd9a39ff9e07542b5b266a387d5a9d1141
Shubham Patil <shubhampatilsd@gmail.com>
2021-10-23 23:05:31 -0700

    storing changes here. unsuccessful in getting projects to open in editor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use serde::Deserialize;
 use std::env;
 use std::fs;
-use toml::Value;
+use std::path::Path;
+use std::process::Command;
 
 #[path = "./read_config/read_config.rs"]
 mod read_config;
@@ -9,7 +10,6 @@ mod read_config;
 #[derive(Deserialize)]
 struct TOMLData {
     directory_shortcuts: Vec<ShortcutMetaData>,
-    test: String,
 }
 
 #[derive(Deserialize)]
@@ -24,15 +24,24 @@ fn main() {
 
     //println!("{}", home_directory::get_home_dir());
 
-    let configStr = read_config::get_config_file();
+    let config_str = read_config::get_config_file();
 
-    let config: TOMLData = toml::from_str(&configStr).unwrap();
+    let config: TOMLData = toml::from_str(&config_str).unwrap();
 
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1]);
+    //println!("{}", args[1]);
 
     for i in &config.directory_shortcuts {
-        println!("{:?}", i.name);
+        if i.name == args[1].to_string() {
+            let path = Path::new(&i.path);
+            let newPath = path.to_str().unwrap();
+            println!("{} {}", i.editor_alias, i.path);
+            Command::new(&i.editor_alias)
+                .args([&i.path])
+                .spawn()
+                .expect("Unable to open project");
+            //let output = Command::new("whoami").output().expect("failed to do so");
+        }
     }
 
     //println!("{:?}", config.directory_shortcuts);
