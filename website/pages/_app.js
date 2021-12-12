44a7b4d985d777a215a5b43f405b5720ecb0061a
Shubham Patil <shubhampatilsd@gmail.com>
2021-12-12 14:44:35 -0800

    website meta tag stuff

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>CodeOpen</title>
+        <meta charSet="UTF-8" />
+        <meta property="og:image" content="/mango.png" />
+        <meta
+          name="description"
+          content="A productivity tool that helps you to open your programming projects faster and simpler, directly from the command line!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- website/pages/docs.js
+++ website/pages/docs.js
@@ -2,10 +2,14 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nord } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { Navbar } from "../components/Nav";
+import Head from "next/head";
 
 export default function Docs(props) {
   return (
     <>
+      <Head>
+        <title>CodeOpen - Docs</title>
+      </Head>
       <Navbar />
       <div className="p-8 md:p-16">
         <ReactMarkdown
